38f5393d8cd101fc815665039c0f1a2197eb468d
alepacc <alessiopacconi@gmail.com>
2026-01-27 17:45:34 +0100

    feat: implement theme light/dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,45 @@
-import { faSun} from "@fortawesome/free-solid-svg-icons";
-import { faMoon } from "@fortawesome/free-regular-svg-icons";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
+  const savedTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState<"light" | "dark">(
+    savedTheme === "dark" ? "dark" : "light",
+  );
 
   function handleTheme() {
-      console.log("theme");
-      // TODO: theme 
+    setTheme(theme === "light" ? "dark" : "light");
   }
 
-  return(
-      <section className="header">
-        <h3 className="header__title" onClick={() => navigate('/')}>Where in the world?</h3>
-        <button className="header__button light-mode">
-          <FontAwesomeIcon icon={faMoon} style={{ rotate: "-30deg" }}  onClick={
-            (e) => 
-            {e.stopPropagation();
-            handleTheme();
-          }
-          }/>
-          Dark Mode
-        </button>
-        <button className="header__button dark-mode">
-          <FontAwesomeIcon icon={faSun} style={{ color: "black" }} />
-          Light Mode
-        </button>
-      </section>
-  )
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme === "dark");
+    // save theme value in local storage
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <section className="header">
+      <h3 className="header__title" onClick={() => navigate("/")}>
+        Where in the world?
+      </h3>
+      <button className="header__button" onClick={handleTheme}>
+        {theme == "dark" ? (
+          <>
+            <FontAwesomeIcon icon={faSun} />
+            Light Mode
+          </>
+        ) : (
+          <>
+            <FontAwesomeIcon icon={faMoon} style={{ rotate: "-30deg" }} />
+            Dark Mode
+          </>
+        )}
+      </button>
+    </section>
+  );
 }
 
 export default Header;
